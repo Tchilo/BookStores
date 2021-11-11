8bd3ffea9e7c3c06fb7c914e9dee50e4cb42afa9
Tchilo <tchiloross@gmail.com>
2021-11-11 13:13:34 +0200

    Add reducer function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,32 @@
 const ADD_BOOK = 'BookStores/books/ADD_BOOK';
 const REMOVE_BOOK = 'BookStores/books/REMOVE_BOOK';
 
+const initialState = [];
+
+
+// ACTION CREATORS 
+export const addBook = payload => ({
+    type: ADD_BOOK,
+    payload
+})
+export const removeBook = payload => ({
+    type: REMOVE_BOOK,
+    payload
+})
+
+// REDUCER 
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_BOOK:
+            return[...state, action.payload]
+            break;
+        case REMOVE_BOOK:
+            return state.filter(book => book.id !== id)
+        default:
+            return state
+            break;
+    }
+};
+
+export default reducer;
