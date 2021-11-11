794fcfd128890db73d1da7c262ad6659c5bb6af3
Tchilo <tchiloross@gmail.com>
2021-11-11 16:46:56 +0200

    Add state for book store and add provider to index.js

--- src/components/BookAdd.js
+++ src/components/BookAdd.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
 function BookAdd() {
+
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('')
+  const [author, setauthor] = useState('')
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value)
+  }
+  const submitBookToStore = (e) => {
+    const newBook = {
+      id: uuidv4(),
+      title: e.target.value,
+      author: '',
+    };
+    dispatch(addBook(newBook));
+  };
+
   return (
     <>
       <h2>ADD NEW BOOK</h2>
@@ -13,7 +34,7 @@ function BookAdd() {
           <option value="Romance">Romance</option>
         </select>
       </form>
-      <button type="button">ADD BOOK</button>
+      <button type="button" onClick={(e) => submitBookToStore(e)}>ADD BOOK</button>
     </>
   );
 }
